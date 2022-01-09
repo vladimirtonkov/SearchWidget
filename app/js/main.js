341b24d7a801c6898af7a7834d6e83b0999f5fef
vladimirtonkov <62721503+vladimirtonkov@users.noreply.github.com>
2022-01-09 19:05:38 +0400

    Hidden search widget

--- app/js/main.js
+++ app/js/main.js
@@ -3,19 +3,21 @@ const InnerBlock = document.querySelector('.search__inner')
 const ShowInsertText = document.querySelector('.search__img')
 const Input = document.querySelector('.search__input')
 
-ShowInsertText.addEventListener('click', () => {
+if (ShowInsertText) {
+  ShowInsertText.addEventListener('click', () => {
 
-    if (ShowInsertText.closest('.show-input')) {
-      InnerBlock.classList.remove('show-input')
-      Input.classList.remove('show-placeholder')
-      Input.style.color = '#fff';
+      if (ShowInsertText.closest('.show-input')) {
+        InnerBlock.classList.remove('show-input')
+        Input.classList.remove('show-placeholder')
+        Input.style.color = '#fff';
 
-    } else {
-      InnerBlock.classList.add('show-input')
-      Input.style.color = '#000';
-      Input.classList.add('show-placeholder')
+      } else {
+        InnerBlock.classList.add('show-input')
+        Input.style.color = '#000';
+        Input.classList.add('show-placeholder')
 
-    }
-})  
+      }
+  }) 
+}
 
 
