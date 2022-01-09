388078b6123c1bbc39a49a0d4cf4b8a2ec2dd5bc
Vladimir Tonkov <vovatonkov@yandex.ru>
2022-01-09 19:08:43 +0400

    Hidden search widget

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+
+const InnerBlock = document.querySelector('.search__inner')
+const ShowInsertText = document.querySelector('.search__img')
+const Input = document.querySelector('.search__input')
+
+ShowInsertText.addEventListener('click', () => {
+
+    if (ShowInsertText.closest('.show-input')) {
+      InnerBlock.classList.remove('show-input')
+      Input.classList.remove('show-placeholder')
+      Input.style.color = '#fff';
+
+    } else {
+      InnerBlock.classList.add('show-input')
+      Input.style.color = '#000';
+      Input.classList.add('show-placeholder')
+
+    }
+})  
+
+
